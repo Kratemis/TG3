719c24d37b8f4a1bd6d07e14fad6305bb58f61e9
Sergio Esteban <sergio.esteban@edu.uah.es>
2016-05-10 19:01:00 +0200

    Limpieza del repositorio

--- Sergio y Rebe/primerProyecto/inventario/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
- 
-class CafeForm(forms.Form):
-	Nombre = forms.CharField(max_length=100)
-	Stock = forms.CharField(max_length=100)
-	Descripcion = forms.CharField(widget=forms.Textarea)#campo de tipo textarea 
--- Sergio y Rebe/primerProyecto/inventario/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Cafe(models.Model):
-	Nombre = models.CharField(max_length=100)
-	Stock = models.CharField(max_length=100)
-	Descripcion = models.TextField()	
-
-
